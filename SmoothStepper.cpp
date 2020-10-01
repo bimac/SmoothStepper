23e027b9633a34f4a5c14767a3b9adecc2d41fa5
Florian Rau <bimac@users.noreply.github.com>
2020-10-01 09:43:43 +0200

    initial commit

---
+++ SmoothStepper.cpp
@@ -0,0 +1,122 @@
+/*
+SmoothStepper
+Copyright (C) 2020 Florian Rau
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Arduino.h"
+#include "SmoothStepper.h"
+
+SmoothStepper::SmoothStepper(uint8_t pinStep, uint8_t pinDirection) {
+  _pinStep = pinStep;
+  _pinDirection = pinDirection;
+  pinMode(_pinStep, OUTPUT);
+  pinMode(_pinDirection, OUTPUT);
+}
+
+void SmoothStepper::setPinEnable(uint8_t pinEnable) {
+  _pinEnable = pinEnable;
+  pinMode(_pinEnable, OUTPUT);
+}
+
+void SmoothStepper::setInvertEnable(bool invertEnable) {
+  _invertEnable = invertEnable;
+}
+
+void SmoothStepper::setInvertDirection(bool invertDirection) {
+  _invertDirection = invertDirection;
+}
+
+void SmoothStepper::setAcceleration(float a) {
+  if (a <= 0.0)                               // limit acceleration to positive values
+    return;
+  _a  = a;                                    // Set private variable
+  _c0 = 676000.0 * sqrt(2.0 / a);             // Estimate the first interval (eq. 15)
+}
+
+void SmoothStepper::setMaxSpeed(float vMax) {
+  if (vMax <= 0.0)                            // limit vMax to positive values
+    return;
+  _vMax = vMax;                               // set private variable
+}
+
+void SmoothStepper::setMaxRPM(float maxRPM) {
+  if (maxRPM <= 0.0)                          // limit maxRPM
+    return;
+  _vMax = maxRPM/60.0 * (float)_stepsPerRev;  // set private variable
+}
+
+void SmoothStepper::setStepsPerRev(unsigned int stepsPerRev) {
+  _stepsPerRev = stepsPerRev;
+}
+
+void SmoothStepper::setPulseWidth(unsigned int pulseWidth) {
+  _pulseWidth = pulseWidth;
+}
+
+void SmoothStepper::moveSteps(long nSteps) {
+  float n2;
+  float n3;
+  float ci;
+  float m;
+
+  if (nSteps < 0) {
+    nSteps = nSteps * -1;
+    digitalWrite(_pinDirection, HIGH ^ _invertDirection);
+  }
+  else
+    digitalWrite(_pinDirection, LOW  ^ _invertDirection);
+
+  if (nSteps == 0)                                            // nothing to do for nSteps == 0
+    return;
+
+  if (nSteps == 1) {                                          // a single step doesn't require fancy formulas
+    step();
+    return;
+  }
+
+  // calculate transition points ("linear-factor method")
+  m  = (float) nSteps;
+  n2 = round(_vMax * _vMax / (0.736 * _a));                   // eq24
+  n2 = floor(min(n2, m / 2.0));                               // limit n2 to m/2
+  n3 = nSteps - n2;                                           // n3 is symmetric to n2
+
+  // run the step sequence
+  for (long i = 1; i <= nSteps-1; i++) {
+    if (i == 1)
+      ci = _c0;                                               // first interval
+    else if (i < n2)
+      ci = ci - 2.0*ci/(4.0*(i-1)+1.0) * (n2-i+1.0)/n2;       // acceleration (eq22)
+    else if (i < n3)
+      ci = ci;                                                // top speed
+    else
+      ci = ci - 2.0*ci/(4.0*(i-m)+1.0) * (i-n3)/(m-n3-1.0);   // deceleration (eq25)
+    step();                                                   // step once
+    delayMicroseconds(ci - _pulseWidth);                      // delay for ci microseconds
+  }
+  step();                                                     // final step
+}
+
+void SmoothStepper::enableDriver() {
+  digitalWrite(_pinEnable, HIGH ^ _invertEnable);
+}
+
+void SmoothStepper::disableDriver() {
+  digitalWrite(_pinEnable, LOW  ^ _invertEnable);
+}
+
+void SmoothStepper::step() {
+  digitalWrite(_pinStep, HIGH);
+  delayMicroseconds(_pulseWidth);
+  digitalWrite(_pinStep, LOW);
+}
---
+++ SmoothStepper.h
@@ -0,0 +1,74 @@
+/*
+SmoothStepper
+Copyright (C) 2020 Florian Rau
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SmoothStepper_h
+#define SmoothStepper_h
+
+#include "Arduino.h"
+
+class SmoothStepper {
+  public:
+    // constructor
+    SmoothStepper(uint8_t pinStep, uint8_t pinDirection);
+    
+    // move by n steps
+    void moveSteps(long nSteps);
+
+    // set enable pin
+    void setPinEnable(uint8_t pinEnable);
+
+    // invert enable pin
+    void setInvertEnable(bool invert);
+
+    // invert direction pin
+    void setInvertDirection(bool invert);
+
+    // set the acceleration (steps / s^2)
+    void setAcceleration(float acceleration);
+
+    // set the maximum speed (steps / s)
+    void setMaxSpeed(float maxSpeed);
+
+    // set the maximum speed (rounds / min)
+    void setMaxRPM(float maxRPM);
+
+    // set the number of steps per revolution
+    void setStepsPerRev(unsigned int stepsPerRev);
+
+    // set the duration of step pulses (µs)
+    void setPulseWidth(unsigned int pulseWidth);
+
+    // enables the stepper motor  driver (via _pinEnable)
+    void enableDriver();
+
+    // disables the stepper motor  driver (via _pinEnable)
+    void disableDriver();
+    
+  private:
+    void step();                      // step function
+    uint8_t _pinStep;                 // pin number: step
+    uint8_t _pinDirection;            // pin number: direction
+    uint8_t _pinEnable;               // pin number: enable
+    float _a;                         // acceleration (steps / s^2)
+    float _vMax;                      // maximum speed (steps / s)
+    float _c0;                        // duration of first interval (µs)
+    unsigned int _pulseWidth = 1;     // duration of step pulses (µs)
+    unsigned int _stepsPerRev = 200;  // steps per full revolution
+    bool _invertEnable = false;       // invert the enable pin?
+    bool _invertDirection = false;    // invert the direction pin?
+};
+
+#endif
